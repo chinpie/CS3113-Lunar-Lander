7f6a7ad38b15c784e7847ae6f3d458b9e546839b
Chinpie <git>
2026-03-10 15:09:14 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "CS3113/Entity.h";
+
+/**
+ * Author: Tyler Davidovich
+ * Assignment: Lunar Lander
+ * Date due: 03/14/26
+ * I pledge that I have completed this assignment without
+ * collaborating with anyone else, in conformance with the
+ * NYU School of Engineering Policies and Procedures on
+ * Academic Misconduct.
+ **/
+
+// Enums
+
+// Global Variables
+
+// Local Variables
+
+// Function Declarations
+
+int main(void)
+{
+    return 0;
+}
